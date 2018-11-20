015655b78d857911997ac402436d1ca0374e1e70
celiazemorac <zemorac94@gmail.com>
2018-11-20 16:46:59 +0100

    Añadidos nodos como comentarios

--- src/main/java/es/upm/griseprofundizacion2018/prof2018_CeliaCaroExam/MyString.java
+++ src/main/java/es/upm/griseprofundizacion2018/prof2018_CeliaCaroExam/MyString.java
@@ -15,16 +15,21 @@ public class MyString
 	
     public int compareTo(MyString anotherString) {
     	String second_string = anotherString.getString();
-    	
+    	// nodo 1
     	if(this.string.length() != second_string.length()) {
+    		// nodo 2
     		return -1;
     	}
+    	// nodo 3
     	for(int i = 0; i < this.string.length(); i++) {
     		int value = this.string.charAt(i) - second_string.charAt(i);
+    		// nodo 4
     		if (value != 0) {
+    			// nodo 5
     			return -1;
     		}
     	}
+    	// nodo 6
 		return 0;   	
     }
    
